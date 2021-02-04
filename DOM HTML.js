dcd766508ca964a7fff9ea80d00f3c468cefd80c
Rakhavendra S M <59560301+Rakhavendra99@users.noreply.github.com>
2021-02-04 17:26:10 +0530

    Add files via upload

---
+++ DOM HTML.js
@@ -0,0 +1,13 @@
+
+document.write(Date());
+
+function run(){
+document.getElementById("p1").innerHTML="Good luck";
+
+var a = document.getElementById("id01");
+a.innerHTML="New Heading";
+}
+
+function img(){
+	document.getElementById("myimage").src = "butterfly.jpg";
+}
